3908b0728e57b6295d7113ba0e18fc8b5407d5c6
Deetya Jain <jaindeetya26@gmail.com>
2023-06-22 11:51:46 -0700

    Initial Commit

---
+++ First app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First app
+//
+//  Created by Deetya Jain on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First app/First_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_appApp.swift
+//  First app
+//
+//  Created by Deetya Jain on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct First_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
